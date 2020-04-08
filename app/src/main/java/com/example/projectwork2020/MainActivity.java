dc4d6239a9a82821816afd630b06aca02c3c0a62
maio <maiorano21@gmail.com>
2020-04-08 15:50:14 +0200

    Commit di prova

--- app/src/main/java/com/example/projectwork2020/MainActivity.java
+++ app/src/main/java/com/example/projectwork2020/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.projectwork2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView prova;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // sajdadaohsajdsahj
+
     }
 }
