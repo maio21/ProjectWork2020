930d767a720075f25a6efea172fd1e1fb6419a18
kevin <kevin.munaro@gmail.com>
2020-04-14 14:25:21 +0200

    Inserimento activity ListaMovies (View prinicipale) e inserita ListView

---
+++ app/src/main/java/com/example/projectwork2020/activity/ListaMovies.java
@@ -0,0 +1,16 @@
+package com.example.projectwork2020.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectwork2020.R;
+
+public class ListaMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_movies);
+    }
+}
