7d31ab323ea2ffbc2b465ecda9614e7d2e5128db
maio <maiorano21@gmail.com>
2020-04-14 15:40:50 +0200

    Sistemata classe MovieTableHelper, implementata libreria retrofit e json, aggiunto permesso internet

--- app/src/main/java/com/example/projectwork2020/data/MovieTableHelper.java
+++ app/src/main/java/com/example/projectwork2020/data/MovieTableHelper.java
@@ -9,11 +9,13 @@ public class MovieTableHelper implements BaseColumns {
     public static final String IMG_COPERTINA = "img_copertina";
     public static final String IMG_DESCRIZIONE = "img_descrizione";
     public static final String TITOLO = "titolo";
+    public static final String DESCRIZIONE = "descrizione";
 
     public static final String CREATE = "CREATE TABLE " + TABLE_NAME + " ( " +
             _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             TITOLO + " TEXT, " +
             PAGINA + " INTEGER, " +
+            DESCRIZIONE + " TEXT, " +
             IMG_COPERTINA + " TEXT, " +
             IMG_DESCRIZIONE + " TEXT ) ;";
 
